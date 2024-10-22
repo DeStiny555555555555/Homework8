89f964724bf87a518d64b0025a0eb7363c312cb7
DeStiny555555555555 <nik.okhrimenko.2000@mail.ru>
2024-10-22 19:14:38 +0300

    Rework Homework

--- src/Main.java
+++ src/Main.java
@@ -21,36 +21,43 @@ public class Main {
         int[] numbers3 = new int[]{1, 2, 3};
         int i3;
         for (i = 0; i < numbers3.length; i++) {
-            System.out.print(numbers3[i] + " ");
+            System.out.print(numbers3[i] + ", ");
         }
         System.out.println();
         double[] numbers4 = new double[]{1.57, 7.654, 9.986};
         int i4;
         for (i4 = 0; i4 < numbers1.length; i4++) {
-            System.out.print(numbers1[i4] + " ");
+            System.out.print(numbers1[i4] + ", ");
         }
         System.out.println();
         int[] numbers5 = new int[]{50, 100, 150};
         for (int i5 = 0; i5 < numbers2.length; i5++) {
-            System.out.print(numbers2[i5] + " ");
+            System.out.print(numbers2[i5] + ", ");
         }
         System.out.println();
         System.out.println("Задание 3");
         int[] numbers6 = new int[]{1, 2, 3};
-        int i6;
-        for (i6 = 2; i6 >= 0; i6--) {
-            System.out.print(numbers[i6] + " ");
+        for (int i6 = numbers6.length -1; i6 >= 0; i6--) {
+            System.out.print(numbers6[i6]);
+            if (i6 > 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
         double[] numbers7 = new double[]{1.57, 7.654, 9.986};
-        int i7;
-        for (i7 = 2; i7 >= 0; i7--) {
-            System.out.print(numbers1[i7] + " ");
+        for (int i7 = numbers7.length -1; i7 >= 0; i7--) {
+            System.out.print(numbers1[i7]);
+            if (i7 > 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
         int[] numbers8 = new int[]{50, 100, 150};
-        for (int i8 = 2; i8 >= 0; i8--) {
-            System.out.print(numbers2[i8] + " ");
+        for (int i8 = numbers8.length -1; i8 >= 0; i8--) {
+            System.out.print(numbers2[i8]);
+            if (i8 > 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
         System.out.println("Задание 4");
